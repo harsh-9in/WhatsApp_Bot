9a95f69af22c90bcedbc8194c9d31c713d96f0d5
Harsh Kumar <harshkumar9.in@gmail.com>
2020-10-05 13:50:57 +0530

    features added

--- WhatsApp_Bot.py
+++ WhatsApp_Bot.py
@@ -8,15 +8,18 @@ time.sleep(3)
 
 
 chrome.get("https://web.whatsapp.com/")
-time.sleep(10)
+time.sleep(15)
 
 search_box = chrome.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]')
-name= input()
+
+name = input("Enter the name ")
 search_box.send_keys(name)
 search_box.send_keys(Keys.ENTER)
 
 message_box=chrome.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-for i in range(0,20):
-    message_box.send_keys("happy")
+x=int(input("Enter the count of message  "))
+msg=input("Enter the message  ")
+for i in range(0,x):
+    message_box.send_keys(msg)
     message_box.send_keys(Keys.ENTER)
     time.sleep(1)
